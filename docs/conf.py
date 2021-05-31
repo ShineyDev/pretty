ec38dace113ecb095bf8181c96ab61d7323d4c62
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2021-05-31 15:20:24 +0100

    add documentation configuration

---
+++ docs/conf.py
@@ -0,0 +1,67 @@
+import os
+import re
+import sys
+
+sys.path.insert(0, os.path.abspath(".."))
+
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.extlinks",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.napoleon",
+    "sphinxcontrib_trio",
+    "sphinx_rtd_theme",
+]
+
+autodoc_member_order = "groupwise"
+autodoc_typehints = "none"
+
+extlinks = {
+    "issue": ("https://github.com/ShineyDev/pretty/issues/%s", "#"),
+}
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+}
+
+highlight_language = "python3"
+html_theme = "sphinx_rtd_theme"
+html_theme_options = {
+    "collapse_navigation": False,
+    "includehidden": False,
+    "navigation_depth": -1,
+    "prev_next_buttons_location": None,
+    "titles_only": True,
+}
+master_doc = "index"
+pygments_style = "friendly"
+source_suffix = ".rst"
+
+copyright = "2020-present, ShineyDev"
+project = "pretty"
+
+_version_regex = r"^version = ('|\")((?:[0-9]+\.)*[0-9]+(?:\.?([a-z]+)(?:\.?[0-9])?)?)\1$"
+
+with open("../pretty/__init__.py") as stream:
+    match = re.search(_version_regex, stream.read(), re.MULTILINE)
+
+version = match.group(2)
+
+if match.group(3) is not None:
+    try:
+        import subprocess
+
+        process = subprocess.Popen(["git", "rev-list", "--count", "HEAD"], stdout=subprocess.PIPE)
+        out, _ = process.communicate()
+        if out:
+            version += out.decode("utf-8").strip()
+
+        process = subprocess.Popen(["git", "rev-parse", "--short", "HEAD"], stdout=subprocess.PIPE)
+        out, _ = process.communicate()
+        if out:
+            version += "+g" + out.decode("utf-8").strip()
+    except (Exception) as e:
+        pass
+
+release = version
---
+++ docs/index.rst
@@ -0,0 +1,16 @@
+pretty
+======
+
+A Python module for prettier (and far more useful) stack traces.
+
+
+.. toctree::
+    :maxdepth: 2
+
+    introduction
+
+
+Still can't find what you're looking for?
+
+* :ref:`genindex`
+* :ref:`search`
