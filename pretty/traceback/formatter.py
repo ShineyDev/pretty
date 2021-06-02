abfba04e6203d8172e3d7f7bc67502ce733e59cf
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2021-06-02 16:00:33 +0100

    move things around a little

--- pretty/traceback/formatter.py
+++ pretty/traceback/formatter.py
@@ -21,71 +21,7 @@ class TracebackFormatter(metaclass=abc.ABCMeta):
     def __init__(self, *, theme):
         self.theme = theme
 
-    def _extract_value_traceback(self, type, value, traceback):
-        if (value is _sentinel) != (traceback is _sentinel):
-            raise ValueError("Both or neither of value and tb must be given")
-        elif value is _sentinel:
-            if type is None:
-                return None, None
-            else:
-                return type, type.__traceback__
-        else:
-            return value, traceback
-
-    @utils.wrap(traceback.extract_tb)
-    def _extract_traceback(self, tb, limit=None):
-        return traceback.StackSummary(self.extract_frames(tb, limit=limit))
-
-    if sys.version_info >= (3, 10):
-        @utils.wrap(traceback.format_exception)
-        def _format_exception(self, exc, value=_sentinel, tb=_sentinel, limit=None, chain=True):
-            value, tb = self._extract_value_traceback(exc, value, tb)
-            exc = type(value)
-
-            return list(self.format_exception(exc, value, tb, chain=chain, limit=limit))
-
-        @utils.wrap(traceback.format_exception_only)
-        def _format_exception_only(self, exc, value=_sentinel):
-            value, _ = self._extract_value_traceback(exc, value, None)
-            exc = type(value)
-
-            return list(self.format_exception_only(exc, value))
-    else:
-        @utils.wrap(traceback.format_exception)
-        def _format_exception(self, etype, value, tb, limit=None, chain=True):
-            return list(self.format_exception(type(value), value, tb, chain=chain, limit=limit))
-
-        @utils.wrap(traceback.format_exception_only)
-        def _format_exception_only(self, etype, value):
-            return list(self.format_exception_only(type(value), value))
-
-    @utils.wrap(traceback.format_list)
-    def _format_frames(self, extracted_list):
-        return list(self.format_frames(extracted_list))
-
-    @utils.wrap(traceback.format_tb)
-    def _format_traceback(self, tb, limit=None):
-        return list(self.format_traceback(tb, limit=limit))
-
-    if sys.version_info >= (3, 10):
-        @utils.wrap(traceback.print_exception)
-        def _write_exception(self, exc, value=_sentinel, tb=_sentinel, limit=None, file=None, chain=True):
-            value, tb = self._extract_value_traceback(exc, value, tb)
-            exc = type(value)
-
-            self.write_exception(exc, value, tb, chain=chain, file=file, limit=limit)
-    else:
-        @utils.wrap(traceback.print_exception)
-        def _write_exception(self, etype, value, tb, limit=None, file=None, chain=True):
-            self.write_exception(type(value), value, tb, chain=chain, file=file, limit=limit)
-
-    @utils.wrap(traceback.print_list)
-    def _write_frames(self, extracted_list, file=None):
-        self.write_frames(extracted_list, file=file)
-
-    @utils.wrap(traceback.print_tb)
-    def _write_traceback(self, tb, limit=None, file=None):
-        self.write_traceback(tb, file=file, limit=limit)
+    # region public methods
 
     @abc.abstractmethod
     def extract_frames(self, obj, *, limit=None):
@@ -286,6 +222,77 @@ class TracebackFormatter(metaclass=abc.ABCMeta):
         """
 
         print("".join(self.format_traceback(traceback, limit=limit)), end="", file=file or sys.stderr)
+
+    # endregion
+    # region private methods
+
+    def _extract_value_traceback(self, type, value, traceback):
+        if (value is _sentinel) != (traceback is _sentinel):
+            raise ValueError("Both or neither of value and tb must be given")
+        elif value is _sentinel:
+            if type is None:
+                return None, None
+            else:
+                return type, type.__traceback__
+        else:
+            return value, traceback
+
+    @utils.wrap(traceback.extract_tb)
+    def _extract_traceback(self, tb, limit=None):
+        return traceback.StackSummary(self.extract_frames(tb, limit=limit))
+
+    if sys.version_info >= (3, 10):
+        @utils.wrap(traceback.format_exception)
+        def _format_exception(self, exc, value=_sentinel, tb=_sentinel, limit=None, chain=True):
+            value, tb = self._extract_value_traceback(exc, value, tb)
+            exc = type(value)
+
+            return list(self.format_exception(exc, value, tb, chain=chain, limit=limit))
+
+        @utils.wrap(traceback.format_exception_only)
+        def _format_exception_only(self, exc, value=_sentinel):
+            value, _ = self._extract_value_traceback(exc, value, None)
+            exc = type(value)
+
+            return list(self.format_exception_only(exc, value))
+    else:
+        @utils.wrap(traceback.format_exception)
+        def _format_exception(self, etype, value, tb, limit=None, chain=True):
+            return list(self.format_exception(type(value), value, tb, chain=chain, limit=limit))
+
+        @utils.wrap(traceback.format_exception_only)
+        def _format_exception_only(self, etype, value):
+            return list(self.format_exception_only(type(value), value))
+
+    @utils.wrap(traceback.format_list)
+    def _format_frames(self, extracted_list):
+        return list(self.format_frames(extracted_list))
+
+    @utils.wrap(traceback.format_tb)
+    def _format_traceback(self, tb, limit=None):
+        return list(self.format_traceback(tb, limit=limit))
+
+    if sys.version_info >= (3, 10):
+        @utils.wrap(traceback.print_exception)
+        def _write_exception(self, exc, value=_sentinel, tb=_sentinel, limit=None, file=None, chain=True):
+            value, tb = self._extract_value_traceback(exc, value, tb)
+            exc = type(value)
+
+            self.write_exception(exc, value, tb, chain=chain, file=file, limit=limit)
+    else:
+        @utils.wrap(traceback.print_exception)
+        def _write_exception(self, etype, value, tb, limit=None, file=None, chain=True):
+            self.write_exception(type(value), value, tb, chain=chain, file=file, limit=limit)
+
+    @utils.wrap(traceback.print_list)
+    def _write_frames(self, extracted_list, file=None):
+        self.write_frames(extracted_list, file=file)
+
+    @utils.wrap(traceback.print_tb)
+    def _write_traceback(self, tb, limit=None, file=None):
+        self.write_traceback(tb, file=file, limit=limit)
+
+    # endregion
 
 
 class DefaultTracebackFormatter(TracebackFormatter):
