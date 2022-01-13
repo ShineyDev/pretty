69a0cc41708b4918304628437396981365fc73a8
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2022-01-13 17:47:12 +0000

    add some notes around the pretty_theme definition

--- pretty/utils.py
+++ pretty/utils.py
@@ -30,7 +30,33 @@ def environment_to_boolean(name, default):
             return default
 
 
+# NOTE: SGR color values are HSV(x°, 30%, 100%) where;
+#
+#       x     SGR           name
+#       ---   -----------   ---------
+#       0     255;179;179   red
+#       30    255;217;179   orange
+#       60    255;255;179   yellow
+#       90    217;255;179   lime
+#       120   179;255;179   green
+#       150   179;255;217   turquoise
+#       180   179;255;255   cyan
+#       210   179;217;255   blue
+#       240   179;179;255   indigo
+#       270   217;179;255   purple
+#       300   255;179;255   magenta
+#       330   255;179;217   pink
+#
+#       SGR grayscale values are HSV(0°, 0%, y%) where;
+#
+#       y     SGR           name
+#       --    -----------   -----
+#       10    26;26;26      black
+#       70    179;179;179   gray
+#       90    230;230;230   white
+#
 pretty_theme = {
+    # colorizing abstract syntax trees
     "ast_brace_sgr": ("38;2;179;179;179", "39"),
     "ast_bracket_sgr": ("38;2;179;179;179", "39"),
     "ast_comment_sgr": ("38;2;179;255;179", "39"),
@@ -38,14 +64,17 @@ pretty_theme = {
     "ast_keyword_sgr": ("38;2;179;179;255", "39"),
     "ast_operator_sgr": ("38;2;179;179;179", "39"),
     "ast_parenthesis_sgr": ("38;2;179;179;179", "39"),
+    # customizing character display
     "char_cap": "\u2514",
     "char_pipe": "\u2502",
     "char_quote": "\u0022",
+    # pretty.traceback
     "traceback_sgr": None,
     "traceback_exception_sgr": ("38;2;255;179;179", "39"),
     "traceback_frame_sgr": "1",
     "traceback_introspection_sgr": ("38;2;255;179;255", "39"),
     "traceback_source_sgr": None,
+    # colorizing literal types
     "type_bool_sgr": ("38;2;179;179;255", "39"),
     "type_bytes_sgr": ("38;2;255;217;179", "39"),
     "type_complex_sgr": ("38;2;179;255;255", "39"),
