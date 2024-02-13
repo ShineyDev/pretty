7c50f33cc59c5a88677e4de39f94646e2ba3fd3c
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2024-02-13 16:32:47 +0000

    implement pretty.utility.MISSING

--- pretty/utility/__init__.py
+++ pretty/utility/__init__.py
@@ -1,7 +1,10 @@
 from pretty.utility.everything import *
 from pretty.utility.everything import __all__ as _everything__all__
+from pretty.utility.typing import *
+from pretty.utility.typing import __all__ as _typing__all__
 
 
 __all__ = [
     *_everything__all__,
+    *_typing__all__,
 ]
---
+++ pretty/utility/typing.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import Any
+
+
+class _MissingSentinel:
+    __slots__ = ()
+
+    def __repr__(self) -> str:
+        return "..."
+
+    def __bool__(self) -> bool:
+        return False
+
+
+MISSING: Any = _MissingSentinel()
+
+
+__all__ = [
+    "MISSING",
+]
