a4d1564e60ca87634675f16808bdb983c31aba01
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2024-02-13 21:56:09 +0000

    implement get_environment_logging utility

--- pretty/utility/environment.py
+++ pretty/utility/environment.py
@@ -6,7 +6,11 @@ if TYPE_CHECKING:
 
     _T = TypeVar("_T")
 
+import logging
 import os
+
+from pretty.utility.transform import try_integer
+from pretty.utility.typing import MISSING
 
 
 if TYPE_CHECKING:
@@ -37,6 +41,48 @@ def get_environment(
     return os.environ.get(name, default)
 
 
+if TYPE_CHECKING:
+
+    @overload
+    def get_environment_logging(
+        name: str,
+        /,
+    ) -> int | None:
+        ...
+
+    @overload
+    def get_environment_logging(
+        name: str,
+        /,
+        *,
+        default: _T,
+    ) -> int | _T:
+        ...
+
+
+def get_environment_logging(
+    name: str,
+    /,
+    *,
+    default: _T = None,
+) -> int | _T:
+    value: str = get_environment(name, default=MISSING)
+
+    if value is MISSING:
+        return default
+
+    integer: int = try_integer(value, default=MISSING)
+
+    if integer is not MISSING:
+        return integer
+
+    try:
+        return logging._nameToLevel[value]
+    except KeyError:
+        return default
+
+
 __all__ = [
     "get_environment",
+    "get_environment_logging",
 ]
