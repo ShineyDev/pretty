4253c22e10ba458816bcd534ee0bb5dcc2278141
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2024-02-13 22:00:06 +0000

    implement get_environment_boolean

--- pretty/utility/environment.py
+++ pretty/utility/environment.py
@@ -41,6 +41,49 @@ def get_environment(
     return os.environ.get(name, default)
 
 
+_bool_values_map = {
+    False: ["0", "false", "off", "disable", "no", "n"],
+    True: ["1", "true", "on", "enable", "yes", "y"],
+}
+_value_bool_map = {v: k for k in _bool_values_map.keys() for v in _bool_values_map[k]}
+
+
+if TYPE_CHECKING:
+
+    @overload
+    def get_environment_boolean(
+        name: str,
+        /,
+    ) -> bool | None:
+        ...
+
+    @overload
+    def get_environment_boolean(
+        name: str,
+        /,
+        *,
+        default: _T,
+    ) -> bool | _T:
+        ...
+
+
+def get_environment_boolean(
+    name: str,
+    /,
+    *,
+    default: _T = None,
+) -> bool | _T:
+    value: str = get_environment(name, default=MISSING)
+
+    if value is MISSING:
+        return default
+
+    try:
+        return _value_bool_map[value]
+    except KeyError:
+        return default
+
+
 if TYPE_CHECKING:
 
     @overload
@@ -84,5 +127,6 @@ def get_environment_logging(
 
 __all__ = [
     "get_environment",
+    "get_environment_boolean",
     "get_environment_logging",
 ]
