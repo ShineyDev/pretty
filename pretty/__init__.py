ac0135300d58e957be175a2f1cb7556489a2ebe7
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2024-02-13 21:59:48 +0000

    use logging in pretty._main
    add PYTHONPRETTYLOGGING environment variable

--- pretty/__init__.py
+++ pretty/__init__.py
@@ -8,6 +8,7 @@ from __future__ import annotations
 from typing import NamedTuple
 
 import json
+import logging
 import os
 import sys
 
@@ -28,24 +29,7 @@ version: str = "2.0.0a"
 version_info: _VersionInfo = _VersionInfo(2, 0, 0, "alpha", 0)
 
 
-def _fail(
-    exc_value: Exception,
-    message: str,
-    /,
-) -> None:
-    if hasattr(sys, "last_value") and sys.last_value is not None:
-        _log("ERROR", f"{message}.")
-    else:
-        sys.last_type, sys.last_value, sys.last_traceback = type(exc_value), exc_value, exc_value.__traceback__
-        _log("ERROR", f"{message}. see traceback.print_last().")
-
-
-def _log(
-    level: str,    
-    message: str,
-    /,
-) -> None:
-    print(f"{level}:pretty:{message}")  # TODO: use logging, so that this can be disabled
+logger = utility.get_logger("pretty")
 
 
 def _main() -> None:
@@ -59,6 +43,8 @@ def _main() -> None:
     if enable_all is False:
         return
 
+    logger.setLevel(pretty.utility.get_environment_logging("PYTHONPRETTYLOGGING", default=logging.INFO))
+
     theme = pretty.utility.pretty_theme.copy()
 
     env_theme = os.environ.get("PYTHONPRETTYTHEME")
@@ -66,25 +52,27 @@ def _main() -> None:
         try:
             user_theme = json.loads(env_theme)
         except json.JSONDecodeError as e:
-            _fail(e, "failed to load PYTHONPRETTYTHEME, falling back to default")
+            logger.error("failed to load PYTHONPRETTYTHEME, falling back to default", e)
         else:
             if isinstance(user_theme, dict):
                 theme.update(user_theme)
             else:
-                _log("WARNING", "PYTHONPRETTYTHEME not a mapping, falling back to default")
+                logger.warning("PYTHONPRETTYTHEME not a mapping, falling back to default")
 
     if pretty.utility.try_bool(os.environ.get("PYTHONPRETTYTRACEBACK"), default=enable_all):
         try:
             pretty.traceback.hook(theme=theme)
         except Exception as e:
-            _fail(e, "failed to hook pretty.traceback")
+            logger.error("failed to hook pretty.traceback", e)
+        else:
+            logger.info("hooked pretty.traceback")
 
 
 def _main_catchall() -> None:
     try:
         _main()
     except Exception as e:
-        _fail(e, "failed to initialize pretty")
+        logger.error("failed to initialize pretty", e)
 
 
 __all__ = [
